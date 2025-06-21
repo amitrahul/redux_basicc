53850603c2fea7073c3ee65a71aba4155ce0337d
Rahul Raj <raj.rahul686@gmail.com>
2025-06-22 00:23:31 +0530

    react-redux installed one again and integrate the redux toolkit

--- reduxThings/src/store.js
+++ reduxThings/src/store.js
@@ -1,12 +1,24 @@
 import { createStore, combineReducers } from "redux";
 import todoReducer from "./reducers/todoReducer";
 
+// import { configureStore } from "@reduxjs/toolkit";
+// import todoSlice from "./slices/todoSlice";
+
 const reduxDevToolEnhancers =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 const store = createStore(
   combineReducers({ todos: todoReducer }),
   {},
   reduxDevToolEnhancers
 );
+
+// const store = configureStore({
+//   reducer: {
+//     todos: todoSlice.reducer,
+//   },
+//   devTools: true,
+// });
+// console.log("heello");
 
 export default store;
